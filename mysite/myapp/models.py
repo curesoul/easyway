b6f6228be7f8d938e9d78c5740cd3eab0c41711f
curesoul <curesoul@163.com>
2022-07-31 10:29:41 +0800

    220731-2

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -3,17 +3,26 @@ from django.utils.text import slugify
 from django.urls import reverse
 
 
+class Tag(models.Model):
+    title = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.title
+
+
 class Category(models.Model):
     title = models.CharField(max_length=255, default='')
 
     def __str__(self):
         return self.title
 
+
 class Flower(models.Model):
     title = models.CharField(max_length=255, default='')
     description = models.TextField(default='')
-    category = models.ForeignKey(Category, null=True, on_delete=models.PROJECT)
     slug = models.SlugField(blank=True, default='')
+    category = models.ForeignKey(Category, null=True, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
@@ -24,4 +33,3 @@ class Flower(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', args=[str(self.slug)])
-
