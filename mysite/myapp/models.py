b769cfa9661a13dd99e8125481839878c1c0cf42
curesoul <curesoul@163.com>
2022-07-31 08:47:58 +0800

    220731-1

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
 
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.title
+
 class Flower(models.Model):
     title = models.CharField(max_length=255, default='')
     description = models.TextField(default='')
