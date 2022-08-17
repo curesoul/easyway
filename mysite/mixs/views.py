e7f8d1c268349c611f03d13cf6eda5d3e147d8a6
curesoul <curesoul@163.com>
2022-08-17 23:08:02 +0800

    220808

--- mysite/mixs/views.py
+++ mysite/mixs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Detail
+from .models import Detail, Lot, Item, Operator, Customer
 
 
 def index(request):
@@ -11,9 +11,20 @@ def index(request):
     return render(request, 'mixs/index.html', context)
 
 
+# def lot_list(request):
+#     lots = Detail.objects.filter(lot__lot='220817', item__name='P6011NC')
+#     context = {
+#         "lots": lots,
+#     }
+#     return render(request, 'mixs/detail.html', context)
+
+
 def lot_list(request):
-    lots = Detail.objects.filter(lot__lot='220817', item__name='P6011NC')
+    lots = Lot.objects.all()[0:30]
+    details = Detail.objects.filter(lot__lot=[lots])
     context = {
-        "lots": lots,
+        'lots': lots,
+        'detail': details,
     }
-    return render(request, 'mixs/detail.html', context)
+    return render(request, 'mixs/lot_list.html', context)
+
